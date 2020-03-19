2fa238aa0deaee5652d5dcac9d58e4f40168ea64
Austin Bryant <austindbryant@gmail.com>
2020-03-19 15:28:32 -0700

    Foreign key constraints

--- src/python/entities/invoice.py
+++ src/python/entities/invoice.py
@@ -6,9 +6,9 @@ class Invoice(Base):
     __tablename__ = 'invoice'
 
     invoice_id = Column(Integer, primary_key=True, autoincrement=True)
-    supplier = Column(Integer, nullable=False)
+    supplier = Column(Integer, ForeignKey('supplier.supplier_id'), nullable=False)
     date = Column(Datetime, nullable=False)
-    branch = Column(Integer, nullable=False)
+    branch = Column(Integer, ForeignKey('store.store_id'), nullable=False)
 
     def __repr__(self):
         return "Invoice(id={}, supplier={}, date={}, branch={})".format(
--- src/python/entities/invoice_item.py
+++ src/python/entities/invoice_item.py
@@ -5,11 +5,10 @@ from entities import Base
 class InvoiceItem(Base):
     __tablename__ = 'invoiceItem'
 
-    invoice_id = Column(Integer, primary_key=True, autoincrement=True)
+    invoice_id = Column(Integer, ForeignKey('invoice.invoice_id'), primary_key=True)
     item_id = Column(Integer, primary_key=True)
-    quantity = Column(Integer, nullable=False)
+    quantity = Column(Integer, CheckConstraint('quantity > 0'), nullable=False)
     price = Column(Float(precision='5,2'))
-    CheckConstraint('quantity > 0')
 
     def __repr__(self):
         return "InvoiceItem(invoice={}, item={}, qty={}, price={})".format(
