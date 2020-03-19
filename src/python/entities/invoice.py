bb7c5338cf9f8559f0cdce70b783146968a34d0a
Austin Bryant <austindbryant@gmail.com>
2020-03-19 16:14:38 -0700

    removed broken test

--- src/python/entities/invoice.py
+++ src/python/entities/invoice.py
@@ -11,14 +11,14 @@ class Invoice(Base):
     supplier_id = Column(Integer, ForeignKey('supplier.supplier_id'), nullable=False)
     supplier = relationship("Supplier")
     date = Column(DateTime, default=datetime.datetime.now, nullable=False)
-    store_id = Column(Integer, ForeignKey('store.store_id'), nullable=False)
-    store = relationship("Store")
+    branch_id = Column(Integer, ForeignKey('branch.id'), nullable=False)
+    branch = relationship("Branch")
 
 
     def __repr__(self):
         return "Invoice(id={}, supplier={}, date={}, branch={})".format(
             self.invoice_id,
-            self.supplier,
+            self.supplier_id,
             self.date,
-            self.store_id
+            self.branch_id
         )
--- src/python/test-driver.py
+++ src/python/test-driver.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
 
 from entities import Base
-from entities.store import Store
 from entities.supplier import Supplier
 from entities.invoice import Invoice
 from entities.branch import Branch
@@ -24,25 +23,6 @@ class TestDBSetup(unittest.TestCase):
         session.configure(bind=engine)
         self.this_session = session()
 
-    def test_add_store(self):
-        address = '1 Grand Ave'
-        city = 'San Luis Obispo'
-        state = 'CA'
-        zip_code = 93410
-        new_store = Store(
-            address=address,
-            city=city,
-            state=state,
-            zip_code=zip_code
-        )
-        self.this_session.add(new_store)
-
-        out_store = self.this_session.query(Store).filter_by(store_id=1).first()
-        self.assertEqual(out_store.address, address)
-        self.assertEqual(out_store.city, city)
-        self.assertEqual(out_store.state, state)
-        self.assertEqual(out_store.zip_code, zip_code)
-
     def test_add_employees(self):
         manager_role = Role(
             name=RoleName.BRANCH_MANAGER,
@@ -60,7 +40,7 @@ class TestDBSetup(unittest.TestCase):
             state='CA',
             zip_code=93410
         )
-        
+
         manager_ssn = 555555555
         manager_name = 'Joe'
         manager_info = EmployeeInfo(
@@ -98,7 +78,7 @@ class TestDBSetup(unittest.TestCase):
             type=Exemption.EXEMPT,
             rate=50000.00
         )
-        
+
         manager_ssn = 555555555
         manager_name = 'Joe'
         manager_info = EmployeeInfo(
@@ -149,16 +129,16 @@ class TestDBSetup(unittest.TestCase):
 
     def test_add_invoice(self):
         supplier = 1
-        store = 1
+        branch = 1
         new_invoice = Invoice(
             supplier_id=supplier,
-            store_id=store
+            branch_id=branch
         )
         self.this_session.add(new_invoice)
 
         out_invoice = self.this_session.query(Invoice).filter_by(invoice_id=1).first()
         self.assertEqual(out_invoice.supplier_id, supplier)
-        self.assertEqual(out_invoice.store_id, store)
+        self.assertEqual(out_invoice.branch_id, branch)
 
 if __name__ == '__main__':
     unittest.main()
