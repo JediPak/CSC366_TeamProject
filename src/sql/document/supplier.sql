e61de67c3506e313a223d992de2448144953f1d4
Austin Bryant <austindbryant@gmail.com>
2020-03-20 16:25:22 -0700

    Remove branch joining, copy format from relational for 2

--- src/sql/document/supplier.sql
+++ src/sql/document/supplier.sql
@@ -1,8 +1,6 @@
 -- Supplier I
 SELECT invoice ->> branch_id, item ->> item_id, MIN(invoice - >>order_date)
 FROM invoice
-JOIN branch
-    ON invoice ->> branch_id = branch ->> id
 JOIN invoice_item
     ON invoice ->> invoice_id = invoice_item ->> invoice_id
 JOIN item
@@ -10,13 +8,26 @@ JOIN item
 GROUP BY invoice ->> branch_id, item ->> id
 ORDER BY invoice ->> branch_id, item ->> id
 
+
 -- Supplier II: Average time between orders for each branch
-SELECT invoice ->> branch_id,
-    AVG(TO_DATE(invoice ->> order_date, 'YYYY-MM-DD') - TO_DATE(LAG(invoice ->> order_date, 1), 'YYYY-MM-DD'))
-        over (order by invoice ->> order_date)
-FROM invoice
-GROUP BY invoice ->> branch_id
-ORDER BY  invoice ->> branch_id, invoice ->> order_date
+WITH
+
+time_between as (
+SELECT
+    invoice ->> branch_id,
+    (TO_DATE(invoice ->> order_date, 'YYYY-MM-DD') - TO_DATE(LAG(invoice ->> order_date), 'YYYY-MM-DD') over
+        (partition by invoice ->> branch_id order by invoice ->> order_date asc)) time_between_ship
+FROM
+    invoice i
+)
+
+SELECT
+    invoice ->> branch_id,
+    avg(time_between_ship) as avg_time_between_ship
+FROM
+    time_between
+GROUP BY invoice ->> branch_id;
+
 
 -- Supplier III: Orders not delivered within 7 days
 SELECT invoice ->> invoice_id as id
