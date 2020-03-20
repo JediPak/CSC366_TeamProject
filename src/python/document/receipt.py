65bf05549dc1cda9e738dbd35b1926b68d476188
lreckard <luke.reckard@gmail.com>
2020-03-19 23:18:00 -0700

    initial document classes

---
+++ src/python/document/receipt.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Column, Integer
+from sqlalchemy.dialects.postgresql import JSON
+
+from document import Base, TableNames
+
+from jsonschema import validate, ValidationError
+
+class Receipt(Base):
+    __tablename__ = TableNames.RECEIPT.value
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    receipt = Column(JSON, nullable=False)
+
+    SCHEMA = {
+        'type' : 'object',
+        'properties' : {
+            'number' : {
+                'type' : 'number'
+            },
+            'time' : {
+                'type' : 'string'
+            },
+            'branch_id' : {
+                'type' : 'integer'
+            }
+        }
+    }
+
+    @staticmethod
+    def factory(json_dict):
+        try:
+            validate(json_dict, Receipt.SCHEMA)
+            return Receipt(
+                receipt = json_dict
+            )
+        except ValidationError:
+            return None
+                    
