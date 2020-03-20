183cbe90bb326dd40f1fa01ac4a97bd8eaa810d2
Tyler Davis <tyler.davis277@outlook.com>
2020-03-20 14:53:56 -0700

    Remove alt

--- src/sql/relational/supplier.sql
+++ src/sql/relational/supplier.sql
@@ -32,16 +32,6 @@ FROM
     time_between
 GROUP BY branch_id;
 
--- Supplier II ALT
-SELECT invoice_id,
-    AVG(LAG(order_date,1))
-        over (
-            partition by branch_id
-            order by order_date
-        )
-FROM invoice
-ORDER BY branch_id, order_date
-
 -- Supplier III
 SELECT 
     invoice_id
